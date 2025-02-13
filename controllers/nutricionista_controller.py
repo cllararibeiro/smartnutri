4f0d849992546795b7957125932d682611c6210b
Maria Clara <143030148+cllararibeiro@users.noreply.github.com>
2025-02-13 15:32:45 -0300

    Update nutricionista_controller.py

--- controllers/nutricionista_controller.py
+++ controllers/nutricionista_controller.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
 from database.config import Session
 from datetime import datetime
+from sqlalchemy.exc import IntegrityError
 from models.models import Nutricionista, Paciente, Consulta, Dieta
 
 nutricionista_bp = Blueprint('nutricionista', __name__, url_prefix='/nutricionista', template_folder='templates')
@@ -34,15 +35,19 @@ def cadastro_paciente():
     if request.method == 'POST':
         nome = request.form['nome']
         email = request.form['email']
-        data_nasc = request.form['data_nasc']
-        idade = request.form['idade']
-        sexo = request.form['sexo']
-        tel = request.form['tel']
+        data_nasc = request.form.get('data_nasc')
+        sexo = request.form.get('sexo')  
+        tel = request.form['telefone']
         cpf = request.form['cpf']
         doencas_preexistentes = request.form['doencas_preexistentes']
         historico_familiar = request.form['historico_familiar']
     
-        paciente = Paciente(pac_nome=nome,pac_email=email,pac_data_nasc=datetime.strptime(data_nasc, '%Y-%m-%dT%H:%M'),pac_idade=idade,
+        data_nasc_obj = datetime.strptime(data_nasc, '%Y-%m-%d') 
+        
+        hoje = datetime.today()
+        idade = hoje.year - data_nasc_obj.year - ((hoje.month, hoje.day) < (data_nasc_obj.month, data_nasc_obj.day))
+
+        paciente = Paciente(pac_nome=nome,pac_email=email,pac_data_nasc=data_nasc_obj,pac_idade=idade,
             pac_sexo=sexo,pac_tel=tel,pac_cpf=cpf,pac_doencas_preexistentes=doencas_preexistentes,
             pac_historico_familiar=historico_familiar,pac_nutri_id=current_user.nutri_id)
         
@@ -50,7 +55,7 @@ def cadastro_paciente():
             session.add(paciente)
             session.commit()
             flash("Paciente cadastrado com sucesso!", "success")
-            return redirect(url_for('nutricionista.dashboard'))
+            return redirect(url_for('nutricionista.funcionalidades'))
         except IntegrityError:
             session.rollback()
             flash("Erro: Já existe um paciente com esse e-mail ou telefone.", "danger")

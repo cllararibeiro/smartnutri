6ce0eede7a50471bdbc0d8b0c9050d62cf263534
Maria Clara <143030148+cllararibeiro@users.noreply.github.com>
2025-02-26 12:04:35 -0300

    Update script.sql

--- database/script.sql
+++ database/script.sql
@@ -96,5 +96,6 @@ CREATE TABLE tb_dados_antro (
     dad_dobra_peitoral DECIMAL(5,2) DEFAULT NULL,
     dad_dobra_coxa DECIMAL(5,2) DEFAULT NULL,
     dad_dobra_axilar DECIMAL(5,2) DEFAULT NULL,
+    dad_tmb DECIMAL(6,2) DEFAULT NULL,
     FOREIGN KEY (dad_con_id) REFERENCES tb_consultas(con_id)
 );

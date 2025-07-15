8cefe5f25d159c757e774756a45c8c313e387a71
LaunHi <h4c3rshazard2@gmail.com>
2025-07-15 07:54:29 -0300

    arrumando a tabela da dieta

--- database/config.py
+++ database/config.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,DeclarativeBase
 
-engine = create_engine('mysql+pymysql://root:@localhost:3307/db_smartnutri')
+engine = create_engine('mysql+pymysql://root:@localhost:3306/db_smartnutri')
 Session = sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
